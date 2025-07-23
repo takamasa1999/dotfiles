a7bbb1d542482f1b144b157ccc616536569a1cf6
Takamasa Sato <taka@repainter.net>
2025-07-23 16:48:16 +0900

    --no

---
+++ zsh/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ~/config/zsh/zshrc ~/.zshrc
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
