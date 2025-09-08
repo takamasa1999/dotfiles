00102e8bd7862a5054c329aa2a8b6dc1750f03bd
Takamasa <tsbeatbox193@gmail.com>
2025-09-08 23:23:10 +0900

    yazi update, and a bit of nvim

--- nvim/lua/plugins/explorer.lua
+++ nvim/lua/plugins/explorer.lua
@@ -1,4 +1,4 @@
-return {
+local tree = {
 	"nvim-tree/nvim-tree.lua",
 	version = "1.14.0",
 	lazy = false,
@@ -28,6 +28,24 @@ return {
 
 		local keymap = vim.keymap.set
 		-- keymap("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>", { desc = "Toggle explorer" })
-		keymap("n", "<A-e>", "<Cmd>NvimTreeToggle<CR>", { desc = "Toggle explorer" })
+		keymap({ "n", "i", "v" }, "<A-e>", "<esc><Cmd>NvimTreeToggle<CR>", { desc = "Toggle explorer" })
 	end,
 }
+
+local oil = {
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+	config = function()
+		require("oil").setup()
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
+}
+
+return { tree, oil }
