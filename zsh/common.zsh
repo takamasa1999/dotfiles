27e2dbc9782295998e590afbba5002753e3daba7
Takamasa <tsbeatbox193@gmail.com>
2025-09-13 16:57:12 +0900

    nvim better integration with zsh

--- zsh/common.zsh
+++ zsh/common.zsh
@@ -1,23 +1,18 @@
 export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="gnzh"
+EDITOR="nvim"
 
-plugins+=(vi-mode)
 plugins+=(zsh-autosuggestions)
 plugins+=(zsh-syntax-highlighting)
+plugins+=(zsh-vi-mode)
 
 # This must be called after local settings
 source $ZSH/oh-my-zsh.sh
 
-VI_MODE_SET_CURSOR=true
-VI_MODE_CURSOR_NORMAL=2
-VI_MODE_CURSOR_VISUAL=2
-VI_MODE_CURSOR_INSERT=6
-VI_MODE_CURSOR_OPPEND=0
-
 # neovim is life
-alias n='nvim'
-export MANPAGER='nvim +Man!'
+alias n="nvim"
+export MANPAGER="nvim +Man!"
 
 # for mosh connection
 export LANG="en_US.UTF-8"
