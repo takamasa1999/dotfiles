beb256acb8704b52d4184ea7bbaf8e7309b6a0ea
Takamasa <tsbeatbox193@gmail.com>
2025-09-17 13:36:07 +0900

    zsh theme update

--- zsh/common.zsh
+++ zsh/common.zsh
@@ -1,6 +1,6 @@
 export ZSH="$HOME/.oh-my-zsh"
 
-ZSH_THEME="gnzh"
+# ZSH_THEME="robbyrussell"
 EDITOR="nvim"
 
 plugins+=(zsh-autosuggestions)
@@ -17,3 +17,12 @@ export MANPAGER="nvim +Man!"
 # for mosh connection
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
+
+# Custom theme
+PROMPT=$'%{$fg_bold[green]%}%n@%m %{$reset_color%}%{$fg[white]%}[%~]%{$reset_color%} $(git_prompt_info)\
+%{$fg_bold[blue]%} $%{$reset_color%} '
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[green]%}("
+ZSH_THEME_GIT_PROMPT_SUFFIX=")%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}*%{$fg[green]%}"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
