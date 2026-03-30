4242caf50382984a8f2564efa035a7e44355fb8d
Takamasa Sato <takamasa.sato@systemi.co.jp>
2026-03-30 10:00:17 +0900

    from m4

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,15 +1,29 @@
 -- https://github.com/iamcco/markdown-preview.nvim
+-- return {
+-- 	"iamcco/markdown-preview.nvim",
+-- 	version = "0.0.10",
+-- 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+-- 	build = "cd app && npm install",
+-- 	init = function()
+-- 		vim.g.mkdp_filetypes = { "markdown" }
+-- 	end,
+-- 	ft = { "markdown" },
+-- 	config = function()
+-- 		local keymap = vim.keymap.set
+-- 		keymap("n", "<localleader>p", "<Cmd>MarkdownPreview<CR>", { desc = "Preview Markdown" })
+-- 	end,
+-- }
 return {
-	"iamcco/markdown-preview.nvim",
-	version = "0.0.10",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && npm install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	ft = { "markdown" },
-	config = function()
-		local keymap = vim.keymap.set
-		keymap("n", "<localleader>p", "<Cmd>MarkdownPreview<CR>", { desc = "Preview Markdown" })
-	end,
+  "selimacerbas/markdown-preview.nvim",
+  dependencies = { "selimacerbas/live-server.nvim" },
+  config = function()
+    require("markdown_preview").setup({
+      -- all optional; sane defaults shown
+      instance_mode = "takeover",  -- "takeover" (one tab) or "multi" (tab per instance)
+      port = 0,                    -- 0 = auto (8421 for takeover, OS-assigned for multi)
+      open_browser = true,
+      debounce_ms = 300,
+	  mermaid_renderer = "js",
+    })
+  end,
 }
